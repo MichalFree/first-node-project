804c857cee754b23915bc4eac3b577a4cec113e7
Michał Szymański <michal.szymanski1908@gmail.com>
2022-02-23 15:18:51 +0100

    Fix Task

--- app.js
+++ app.js
@@ -11,23 +11,27 @@ const randChoice = (arr) => {
 
 const people = [];
 
-for (let i=0; i<20; i++) {
+for (let i = 0; i < 20; i++) {
   const gender = randChoice(genders);
   const firstName = gender === 'male' ? randChoice(maleNames) : randChoice(femaleNames);
   const lastName = randChoice(lastNames);
   const age = Math.floor(Math.random() * 60) + 18;
+  const now = new Date();
+  const vintagePerson = now.getFullYear() - age;
+  const eMail = firstName + "." + lastName + vintagePerson + "@gmail.com";
   const person = {
     gender: gender,
     firstName: firstName,
     lastName: lastName,
     age: age,
+    eMail: eMail,
   }
   people.push(person);
 }
 
 const arrayToJSON = JSON.stringify(people);
 
-  fs.writeFile('people.json', arrayToJSON, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+fs.writeFile('people.json', arrayToJSON, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
